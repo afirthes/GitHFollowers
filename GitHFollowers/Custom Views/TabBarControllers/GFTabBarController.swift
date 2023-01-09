f4ded4ac7ee66b318622a6d76b25bf89c0d4f4a2
Sergio <afirthes@gmail.com>
2023-01-09 18:19:59 +0300

    minor changes

---
+++ GitHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  GFTabBarController.swift
+//  GitHFollowers
+//
+//  Created by Afir Thes on 09.01.2023.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        self.viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC        = SearchVC()
+        searchVC.title      = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC         = FavoritesListVC()
+        favoritesListVC.title       = "Favorites"
+        favoritesListVC.tabBarItem  = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+
+}
--- GitHFollowers/Support/SceneDelegate.swift
+++ GitHFollowers/Support/SceneDelegate.swift
@@ -11,54 +11,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        configureNavigationBar()
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = createTabbar()
+        window                      = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene         = windowScene
+        window?.rootViewController  = GFTabBarController()
         window?.makeKeyAndVisible()
-    }
-    
-    func createSearchNC() -> UINavigationController {
-        let searchVC = SearchVC()
-        searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        let searchNc = UINavigationController(rootViewController: searchVC)
-//        searchNc.navigationBar.backgroundColor = .systemGray6
-        return searchNc
-    }
-    
-    func createFavoritesNC() -> UINavigationController {
-        let favoritesVC = FavoritesListVC()
-        favoritesVC.title = "Favorites"
-        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        let favoritesNC = UINavigationController(rootViewController: favoritesVC)
-//        favoritesNC.navigationBar.backgroundColor = .systemGray6
-        return favoritesNC
-    }
-    
-    func createTabbar() -> UITabBarController {
-        let tabbar = TabBarController()
         
-        tabbar.viewControllers = [
-            createSearchNC(),
-            createFavoritesNC()
-        ]
-        
-        return tabbar
+        configureNavigationBar()
     }
+
+    
     
     func configureNavigationBar() {
         UINavigationBar.appearance().tintColor = .systemGreen
     }
 
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -81,7 +58,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
 }
 
